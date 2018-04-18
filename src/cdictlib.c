f91320e630846f8bb0e2262d476bb43c47ba03bb
Kyle Sammons <kyle@firstfoundry.co>
2018-04-17 17:21:03 -0700

    I'm a dummy, fixed single pointer

--- src/cdictlib.c
+++ src/cdictlib.c
@@ -1,44 +1,68 @@
 #include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
 #include "cdictlib.h"
 
 // TODO: Improve this hash function
 int getHash(char * key) {
     int sum = 0;
-    while(key != NULL) {
+    while(*key) {
         sum += *key;
         key++;
     }
     return sum;
 }
 
-void * initDictionary(Dictionary * targetDict) {
+Dictionary * newDictionary() {
     int initialSize = 256;
-    targetDict->contents = (char *)malloc(sizeof(char *) * initialSize);
+
+    Dictionary * targetDict = (Dictionary*)malloc(sizeof(Dictionary));
+    targetDict->contents = (char **)malloc(sizeof(char **) * initialSize);
+
+    if(targetDict->contents == NULL) {
+        printf("[ERROR]: Couldn't initialize dictionary.");
+    }
+
     targetDict->totalSize = initialSize;
+    return targetDict;
 }
 
-void * initDictionaryWithSize(Dictionary * targetDict, int initialSize) {
-    targetDict->contents = (char *)malloc(sizeof(char *) * initialSize);
+Dictionary * newDictionaryWithSize(int initialSize) {
+    Dictionary * targetDict = (Dictionary*)malloc(sizeof(Dictionary));
+    targetDict->contents = (char **)malloc(sizeof(char **) * initialSize);
+
+    if(targetDict->contents == NULL) {
+        printf("[ERROR]: Couldn't initialize dictionary.");
+    }
+
     targetDict->totalSize = initialSize;
+    return targetDict;
 }
 
 char * getContents(Dictionary * targetDict, char * key) {
-    int hash = getHash(*key);
-    int index = hash % targetDict->currentSize;
+    int hash = getHash(key);
+    int index = hash % targetDict->totalSize;
     return targetDict->contents[index];
 }
 
 void setContents(Dictionary * targetDict, char * key, char * value) {
-
     // Double this every time we hit the limit
     if(targetDict->currentSize == targetDict->totalSize) {
-        targetDict->contents = (char *)malloc(sizeof(char * ) * targetDict->totalSize * 2);
+        // Malloc should overwrite our memory with 0's, so just create a temp copy and swap
+        char ** currentContents = targetDict->contents;
+        targetDict->contents = (char **)malloc(sizeof(char ** ) * targetDict->totalSize * 2);
+        memcpy(targetDict->contents, currentContents, sizeof(char *)*targetDict->totalSize);
+
         targetDict->totalSize = targetDict->totalSize * 2;
     }
 
-    int hash = getHash(*key);
-    int index = hash % targetDict->currentSize;
-    targetDict->contents[index] = *value;
+    int hash = getHash(key);
+    int index = hash % targetDict->totalSize;
+    printf("Setting index %d to value %s\n", index, value);
+
+    targetDict->contents[index] = (char *)malloc(sizeof(char) * strlen(*value));
+    strncpy(targetDict->contents[index], *value, sizeof(*value));
+    printf("Getting test: %s\n", targetDict->contents[index]);
     targetDict->currentSize++;
 }
 
--- src/main.c
+++ src/main.c
@@ -2,9 +2,9 @@
 #include <stdio.h>
 
 int main(int argc, char** argv) {
-    Dictionary * dictionary;
-    initDictionary(dictionary);
+    Dictionary * dictionary = newDictionary();
     printf("Dictionary initalized.\n");
+    setContents(dictionary, "Test", "Other test");
     printf("Output: %s\n", getContents(dictionary, "Test"));
     freeDictionary(dictionary);
     return 0;
